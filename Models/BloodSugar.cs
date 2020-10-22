ec5bf62f5f37c8b73a64225c228fbb9828efe634
srclarke <srclarke@mchsi.com>
2020-10-21 20:59:56 -0500

    Initial commit

---
+++ Models/BloodSugar.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blood_Sugars.Models
+{
+    public class BloodSugar
+    {
+
+        public int BloodSugarId { get; set; }
+        public User UserName { get; set; }
+        public int BloodSugars { get; set; }
+        public string Protein { get; set; }
+        public string Veggie { get; set; }
+        public string Fruit { get; set; }
+        public string Drink { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public BeforeAfter BsMealEatin { get; set; }
+
+
+        public enum User { Scott, Kolleen }
+
+        public enum BeforeAfter { Before_Meal, After_Meal }
+    }
+}
